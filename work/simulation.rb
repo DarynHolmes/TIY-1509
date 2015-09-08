e1ae6f45bef0f5fdbe460d6be89c0ddfbc9fb6e8
DarynHolmes <DarynHolmes@gmail.com>
2015-09-08 20:18:58 +0100

    Removed the card that is not the jack

--- work/simulation.rb
+++ work/simulation.rb
@@ -13,6 +13,15 @@ cards << card_3
 index_of_jack = rand(cards.length)
 cards[index_of_jack] = :jack
 
+index_of_guess = rand(cards.length)
+guessed_card = cards.delete_at(index_of_guess)
 
+
+index_of_card_to_discard = cards.find_index(:joker)
+cards.delete_at(index_of_card_to_discard)
 
 puts cards
+puts "The card guessed is #{guessed_card}"
+puts "The remaining card is #{cards}"
+
+
