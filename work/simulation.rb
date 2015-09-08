6b49e8d2914bdc15cc9bca95af84329fc0254899
DarynHolmes <DarynHolmes@gmail.com>
2015-09-08 19:44:12 +0100

    Shuffle and choose winning card

--- work/simulation.rb
+++ work/simulation.rb
@@ -1,2 +1,19 @@
 puts "Welcome to the Simulator!!!!!"
 
+card_1 = :joker
+card_2 = :joker
+card_3 = :joker
+
+cards = []
+
+cards << card_1
+cards << card_2
+cards << card_3
+
+index_of_jack = rand(cards.length)
+cards[index_of_jack] = :jack
+
+cards = cards.shuffle
+
+
+puts cards
