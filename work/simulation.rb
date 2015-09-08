62d33e7edb4cffc468d4248c4b399e76ab5a8807
DarynHolmes <DarynHolmes@gmail.com>
2015-09-08 20:31:13 +0100

    Added an initial loop

--- work/simulation.rb
+++ work/simulation.rb
@@ -1,27 +1,32 @@
 puts "Welcome to the Simulator!!!!!"
 
-card_1 = :joker
-card_2 = :joker
-card_3 = :joker
+5.times do 
 
-cards = []
+	card_1 = :joker
+	card_2 = :joker
+	card_3 = :joker
 
-cards << card_1
-cards << card_2
-cards << card_3
+	cards = []
 
-index_of_jack = rand(cards.length)
-cards[index_of_jack] = :jack
+	cards << card_1
+	cards << card_2
+	cards << card_3
 
-index_of_guess = rand(cards.length)
-guessed_card = cards.delete_at(index_of_guess)
+	index_of_jack = rand(cards.length)
+	cards[index_of_jack] = :jack
 
+	index_of_guess = rand(cards.length)
+	guessed_card = cards.delete_at(index_of_guess)
 
-index_of_card_to_discard = cards.find_index(:joker)
-cards.delete_at(index_of_card_to_discard)
+	index_of_card_to_discard = cards.find_index(:joker)
+	cards.delete_at(index_of_card_to_discard)
 
-puts cards
-puts "The card guessed is #{guessed_card}"
-puts "The remaining card is #{cards}"
+	puts "The card guessed is #{guessed_card}"
+	puts "The remaining card is #{cards}"
 
-
+	if (guessed_card == :jack)
+		puts "You should not swap, you guessed correctly"
+	else
+		puts "You should swap, you did not guess the Jack"
+	end
+end
